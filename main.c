432bbeea6766f9372b677bd69b93d8c0d53f2c99
Marcos <marcospgp26@gmail.com>
2023-09-19 22:10:59 -0300

    Umas mudancas

--- main.c
+++ main.c
@@ -5,11 +5,13 @@
 typedef struct dados
 {
     char CPF[12], Senha[7];//os dois esta com uma casa a mais por causa do fgets
-}Dados;
+} Dados;
+
+void menu(int tipo);
 
 int main()
 {
-    int esc1, ext = 0, endcod = 0;
+    int esc1, err = 0, mn, ext = 0, endcod = 0;
     Dados* info = (Dados *) malloc(sizeof(Dados));
 
     //insercao de dados para entrar no sistema
@@ -26,7 +28,8 @@ int main()
             {
                 printf("Escolha invalida.Por favor reensira-a\n");
             }
-        }while(esc1 < 1 || esc1 > 3) ;
+        }
+        while(esc1 < 1 || esc1 > 3) ;
 
         switch(esc1)
         {
@@ -50,10 +53,11 @@ int main()
                 {
                     //passar verificando cada elemento da lista, para verificar se de fato esse usuario existe e fazer ext = 1 se sim;
                     //para facilitar fazer a funcao de busca retornar os mesmos valores e fazer duas funcoes de busca uma de CPF e uma de Senha
-                    
+
                     ext = 1;
                 }
-            }while(!ext);
+            }
+            while(!ext);
             break;
         case 2:
             do
@@ -76,12 +80,44 @@ int main()
                     //passar verificando cada elemento da lista, para verificar se de fato esse usuario existe e fazer ext = 1 se sim;
                     ext = 1;
                 }
-            }while(!ext);
+            }
+            while(!ext);
             break;
         case 3:
             endcod = 1;
             ext = 1;
+            break;
         }
-    }while(!ext);
+    }
+    while(!ext);
 
+    while(!endcod){
+        err = 0;
+
+        menu(esc1);
+        scanf("%i", &mn);
+
+        if((mn < 0) || (mn > 9)) err++;
+        else if((esc1 == 1) && ((mn > 3) && (mn < 9))) err++;
+        else if((esc1 == 2) && ((mn > 4) && (mn < 9))) err++;
+
+        if(err) printf("Escolha invalida, refaca-a\n");
+        else
+        {
+            switch(mn)
+            {
+            case 9:
+                endcod = 1;
+            }
+        }
+    }
+
+}
+
+void menu(int tipo)
+{
+    printf("Escolha uma opcao:\n\n");
+    printf("1) Acessar o estoque\n2) Acessar o gasto\n3) Acessar a area de compra\n");
+    if(tipo == 2) printf("4) Cadastrar Clientes ou Funcionarios\n");
+    printf("9) Sair\n");
 }
