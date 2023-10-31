aec4d9058b1258502e4cac1ed9a94fc2f8134d7d
brenki2 <pagioliosvaldo@gmail.com>
2023-10-31 16:45:45 -0300

    Arrumando o TAD para ponteiro de funcionario

--- listafuncionarios.h
+++ listafuncionarios.h
@@ -14,7 +14,7 @@ typedef struct lista_f Lista_f;
 
 Lista_f* Criar_f();
 int ListaVazia_f(Lista_f *l);
-int Inserir_inicio_f(Lista_f *l, Funcionario it);
+int Inserir_inicio_f(Lista_f *l, Funcionario *it);
 int Remover_inicio_f(Lista_f *l);
 
 
@@ -23,11 +23,11 @@ int Buscar_Posicao_f(Lista_f *l, int pos);
 
 //===================================================================================================================================================
 void MostrarDadosPagamento(Lista_f *l);
-int MostraOcorrencia_f(Lista_f *l, Funcionario it);
-int InsereFuncionario(Lista_f *l, Funcionario it);
-int VerificarSenha(Lista_f *l, Funcionario it);
-int MudaSalarioFuncionario(Lista_f *l, Funcionario it);
-int MudaSalarioCargo(Lista_f *l, Funcionario it);
+int MostraOcorrencia_f(Lista_f *l, Funcionario *it);
+int InsereFuncionario(Lista_f *l, Funcionario *it);
+int VerificarSenha(Lista_f *l, Funcionario *it);
+int MudaSalarioFuncionario(Lista_f *l, Funcionario *it);
+int MudaSalarioCargo(Lista_f *l, Funcionario *it);
 int MudaCargoFuncionario(Lista_f *l, Funcionario *it);
 
 #endif // LISTAFUNCIONARIOS_H_INCLUDED
